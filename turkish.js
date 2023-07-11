2f8b9db521bef3e1ec3a4f8a47c59d61492b7211
Furkan Baytekin <furkanbaytekin@gmail.com>
2023-07-11 22:01:50 +0300

    refactor: JS compiled with tsc

--- turkish.js
+++ turkish.js
@@ -1,51 +1,54 @@
-export const capitalizeTurkish = (input) => {
-    const words = input.split(" ")
-    const capitalizedWords = words.map((word) => {
-      const firstLetter = word.charAt(0)
-      const restOfWord = word.slice(1).toLocaleLowerCase("tr")
-      return firstLetter.toLocaleUpperCase("tr") + restOfWord
-    })
-    return capitalizedWords.join(" ")
-  }
-
-export const decodeTurkishEntities = (input) => {
-    const entities = {
-      "&Ccedil;": "Ç",
-      "&ccedil;": "ç",
-      "&Uuml;": "Ü",
-      "&uuml;": "ü",
-      "&Ouml;": "Ö",
-      "&ouml;": "ö",
-      "&Iuml;": "İ",
-      "&iuml;": "i",
-      "&Gbreve;": "Ğ",
-      "&gbreve;": "ğ",
-      "&Scedil;": "Ş",
-      "&scedil;": "ş",
-    }
-    return input.replace(/&[A-Za-z]+;/g, (entity) => {
-      return entities[entity] || entity
-    })
-  }
-
-export const decodeTurkishEntitiesInUrl = (url) => {
-  const entities = {
-    "%C3%87": "Ç",
-    "%C3%A7": "ç",
-    "%C3%9C": "Ü",
-    "%C3%BC": "ü",
-    "%C3%96": "Ö",
-    "%C3%B6": "ö",
-    "%C4%B0": "İ",
-    "%C4%B1": "ı",
-    "%C4%9E": "Ğ",
-    "%C4%9F": "ğ",
-    "%C5%9E": "Ş",
-    "%C5%9F": "ş",
-  }
-  const decodedUrl = decodeURIComponent(url)
-  return decodedUrl.replace(/%[A-Fa-f0-9]{2}/g, (entity) => {
-    return entities[entity] || entity
-  })
-}
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.decodeTurkishEntitiesInUrl = exports.decodeTurkishEntities = exports.capitalizeTurkish = void 0;
+var capitalizeTurkish = function (input) {
+    var words = input.split(" ");
+    var capitalizedWords = words.map(function (word) {
+        var firstLetter = word.charAt(0);
+        var restOfWord = word.slice(1).toLocaleLowerCase("tr");
+        return firstLetter.toLocaleUpperCase("tr") + restOfWord;
+    });
+    return capitalizedWords.join(" ");
+};
+exports.capitalizeTurkish = capitalizeTurkish;
+var decodeTurkishEntities = function (input) {
+    var entities = {
+        "&Ccedil;": "Ç",
+        "&ccedil;": "ç",
+        "&Uuml;": "Ü",
+        "&uuml;": "ü",
+        "&Ouml;": "Ö",
+        "&ouml;": "ö",
+        "&Iuml;": "İ",
+        "&iuml;": "i",
+        "&Gbreve;": "Ğ",
+        "&gbreve;": "ğ",
+        "&Scedil;": "Ş",
+        "&scedil;": "ş",
+    };
+    return input.replace(/&[A-Za-z]+;/g, function (entity) {
+        return entities[entity] || entity;
+    });
+};
+exports.decodeTurkishEntities = decodeTurkishEntities;
+var decodeTurkishEntitiesInUrl = function (url) {
+    var entities = {
+        "%C3%87": "Ç",
+        "%C3%A7": "ç",
+        "%C3%9C": "Ü",
+        "%C3%BC": "ü",
+        "%C3%96": "Ö",
+        "%C3%B6": "ö",
+        "%C4%B0": "İ",
+        "%C4%B1": "ı",
+        "%C4%9E": "Ğ",
+        "%C4%9F": "ğ",
+        "%C5%9E": "Ş",
+        "%C5%9F": "ş",
+    };
+    var decodedUrl = decodeURIComponent(url);
+    return decodedUrl.replace(/%[A-Fa-f0-9]{2}/g, function (entity) {
+        return entities[entity] || entity;
+    });
+};
+exports.decodeTurkishEntitiesInUrl = decodeTurkishEntitiesInUrl;
